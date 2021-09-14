d800ebdfa2c36a4cfd4be1e7d3baf8d7beaedf63
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-14 15:01:07 +0200

    add some Links to React Data

--- scripts/data/getDataReact.js
+++ scripts/data/getDataReact.js
@@ -1,3 +1,24 @@
 export const getDataReact = () => {
-  return [{ title: 'React', url: '', info: 'test' }]
+  return [
+    {
+      title: 'What the Fork is the React Virtual DOM',
+      url: 'https://maggieappleton.com/react-vdom',
+      info: 'A short introduction to the concept of Virtual DOM in React',
+    },
+    {
+      title: 'React Styled Components Tutorial',
+      url: 'https://www.robinwieruch.de/react-styled-components',
+      info: 'A tutorial on Styled Components and how to use the in React',
+    },
+    {
+      title: 'Ways of Getting Data from API in React',
+      url: 'https://dev.to/olenadrugalya/ways-of-getting-data-from-api-in-react-2kpf',
+      info: 'This blog post is about the ways of getting data from API in React',
+    },
+    {
+      title: 'React How To',
+      url: 'https://github.com/petehunt/react-howto',
+      info: 'A huge linklist and library on all things React',
+    },
+  ]
 }
