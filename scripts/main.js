634a960abb38cd4eb5c0120e76e487e9d48e0f3f
StefanBehring <chadhz@gmx.de>
2021-09-14 12:06:19 +0200

    Add function for headerBurger to show nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+const headerBurger = document.getElementById('header__burger')
+const navigation = document.getElementById('navigation')
+
+headerBurger.addEventListener('click', () => {
+  console.log('click')
+  navigation.classList.toggle('navigation--hidden')
+})
+
 function init() {
   console.log('*** APP START ***')
 }
