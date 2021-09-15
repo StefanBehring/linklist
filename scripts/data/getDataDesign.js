d55a46b8f833d112e8d2d8fec2640a27f2167968
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-09-15 20:12:28 +0200

    Add links from week two

--- scripts/data/getDataDesign.js
+++ scripts/data/getDataDesign.js
@@ -1,9 +1,84 @@
 export const getDataDesign = () => {
   return [
     {
-      title: 'Test',
-      url: 'https://www.w3schools.com/',
-      info: 'Design test entry',
+      title: 'Google Fonts',
+      url: 'https://fonts.google.com/',
+      info: 'Fonts, fonts and more fonts',
+    },
+    {
+      title: 'Adobe Fonts',
+      url: 'https://fonts.adobe.com/',
+      info: 'In need of a good font?',
+    },
+    {
+      title: 'dafont.com',
+      url: 'https://www.dafont.com/de/',
+      info: 'In case you are wondering where you can find fonts. ;-)',
+    },
+    {
+      title: 'leserlich Kontrastrechner',
+      url: 'https://www.leserlich.info/werkzeuge/kontrastrechner/',
+      info: 'German website where you can check if your contrast fulfills the accessibility criteria',
+    },
+    {
+      title: 'Google Fonts Icons',
+      url: 'https://fonts.google.com/icons',
+      info: 'Google Icons',
+    },
+    {
+      title: 'Material Design Icons',
+      url: 'https://materialdesignicons.com/',
+      info: 'Material Design Icons',
+    },
+    {
+      title: 'Flaticon',
+      url: 'https://www.flaticon.com/',
+      info: 'Icons',
+    },
+    {
+      title: 'Freepik',
+      url: 'https://www.freepik.com/',
+      info: 'Stock images',
+    },
+    {
+      title: 'Unsplah',
+      url: 'https://unsplash.com/',
+      info: 'Stock images',
+    },
+    {
+      title: 'Pexels',
+      url: 'https://www.pexels.com/de-de/',
+      info: 'Stock images',
+    },
+    {
+      title: 'Free Adobe Stock Images',
+      url: 'https://stock.adobe.com/de/free',
+      info: 'Stock images',
+    },
+    {
+      title: 'Adobe Stock Images',
+      url: 'https://stock.adobe.com/',
+      info: 'Stock images',
+    },
+    {
+      title: 'Behance',
+      url: 'https://www.behance.net/',
+      info: 'Network for discovering UX/UI designs',
+    },
+    {
+      title: 'Dribble',
+      url: 'https://dribbble.com/',
+      info: 'Another website for UX and UI designs',
+    },
+    {
+      title: 'Pinterest',
+      url: 'https://www.pinterest.de/',
+      info: 'As inspiration for your UX and UI designs',
+    },
+    {
+      title: 'awwwards',
+      url: 'https://www.awwwards.com/',
+      info: 'Another website as inspiration for your UX and UI designs',
     },
   ]
 }
